db6f60a5fe68c590b3b29da955e034ea15ac3d60
ParkSeokJin1 <134951090+ParkSeokJin1@users.noreply.github.com>
2024-03-04 16:03:50 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {Icon} from './src/components/Icons';
+import {NavigationContainer} from '@react-navigation/native';
+import {RootNavigation} from './src/navigation/RootNavigation';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+export default App;
